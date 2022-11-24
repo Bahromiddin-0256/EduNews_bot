365e71f9334b1ec4dddd33e1e46889101ac1b599
MRprgmr <algoprogram01@gmail.com>
2022-11-24 18:36:00 +0500

    Fixed some errors

--- handlers/private/statistics.py
+++ handlers/private/statistics.py
@@ -3,8 +3,10 @@ from aiogram import Router, types
 from db.crud import show_user_stat
 from db.models import User
 from filters.common import TranslatedText
+from middlewares.base_middlewares import PermissionMiddleware
 
 router = Router()
+router.message.middleware(PermissionMiddleware())
 
 
 @router.message(TranslatedText('statistics'))
