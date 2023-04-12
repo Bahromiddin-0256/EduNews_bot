d8fb9a98e4c5b3fef3a844ed98a9b1279b32efef
MRprgmr <algoprogram01@gmail.com>
2023-04-12 16:57:02 +0500

    make it better

--- handlers/previous_bot.py
+++ handlers/previous_bot.py
@@ -1,9 +1,25 @@
 from aiogram import Router, types
-from .channel.post_actions import router as channel_router
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from filters.chat_type import ChatTypes
+from .channel import router as channel_router
 
-router = Router()
+class PrivateFilter(ChatTypes):
+    chat_types = "private"
+
+main_router = Router()
+private_router = Router()
+
+for observer_key in private_router.observers:
+    private_router.observers[observer_key].filter(PrivateFilter())
 
 
-@router.message()
+@private_router.message()
 async def forward_to_new_bot(message: types.Message):
-    await message.answer("🤖 Bot o'zgardi, iltimos yangi botdan foydalaning: \n\n👉 https://t.me/Media_probot")
+    await message.answer("🤖 Bot o'zgardi, iltimos yangi botdan foydalaning: \n\n👉 @Media_probot", reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="⏩ Botga o'tish", url="https://t.me/Media_probot")
+        ]
+    ]))
+
+main_router.include_router(private_router)
+main_router.include_router(channel_router)
