fe6b5669cb916536646188c15e8fe244ee2d2e0d
MRprgmr <algoprogram01@gmail.com>
2023-04-12 16:23:29 +0500

    Some improvements

--- handlers/previous_bot.py
+++ handlers/previous_bot.py
@@ -1,9 +1,10 @@
 from aiogram import Router, types
-
+from .channel.post_actions import router as channel_router
 
 router = Router()
+router.include_router(channel_router)
 
 
 @router.message()
 async def forward_to_new_bot(message: types.Message):
-    await message.answer("🤖 Bot o'zgardi, iltimos yangi botdan foydalaning: \n\n👉 @Media_probot")
+    await message.answer("🤖 Bot o'zgardi, iltimos yangi botdan foydalaning: \n\n👉 https://t.me/Media_probot")
