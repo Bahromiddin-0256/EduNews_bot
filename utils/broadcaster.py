6d52361385d99fe748a7f978b63a9982fb984eac
MRprgmr <algoprogram01@gmail.com>
2023-04-18 10:38:13 +0500

    Broadcasting to all

--- utils/broadcaster.py
+++ utils/broadcaster.py
@@ -2,10 +2,13 @@ from aiogram.types import Message
 
 
 async def send_copy(message: Message, chat_id: int):
+    sent = False
     try:
         await message.send_copy(chat_id=chat_id)
-        return True
+        sent = True
     except:
+        pass
+    try:
         kwargs = {
             "chat_id": chat_id,
             "reply_markup": message.reply_markup,
@@ -13,31 +16,31 @@ async def send_copy(message: Message, chat_id: int):
         text = message.text or message.caption
         entities = message.entities or message.caption_entities
         from core.misc import bot2
-        try:
-            if message.text:
-                await bot2.send_message(text=message.text, **kwargs)
-            if message.audio:
-                await bot2.send_audio(
-                    audio=message.audio.file_id,
-                    caption=text,
-                    title=message.audio.title,
-                    performer=message.audio.performer,
-                    duration=message.audio.duration,
-                    caption_entities=entities,
-                    **kwargs,
+        if message.text:
+            await bot2.send_message(text=message.text, **kwargs)
+        if message.audio:
+            await bot2.send_audio(
+                audio=message.audio.file_id,
+                caption=text,
+                title=message.audio.title,
+                performer=message.audio.performer,
+                duration=message.audio.duration,
+                caption_entities=entities,
+                **kwargs,
+        )
+        if message.document:
+            await bot2.send_document(
+                document=message.document.file_id, caption=text, caption_entities=entities, **kwargs
             )
-            if message.document:
-                await bot2.send_document(
-                    document=message.document.file_id, caption=text, caption_entities=entities, **kwargs
-                )
-            if message.photo:
-                await bot2.send_photo(
-                    photo=message.photo[-1].file_id, caption=text, caption_entities=entities, **kwargs
-                )
-            if message.video:
-                await bot2.send_video(
-                    video=message.video.file_id, caption=text, caption_entities=entities, **kwargs
-                )
-            return True
-        except:
-            return False
+        if message.photo:
+            await bot2.send_photo(
+                photo=message.photo[-1].file_id, caption=text, caption_entities=entities, **kwargs
+            )
+        if message.video:
+            await bot2.send_video(
+                video=message.video.file_id, caption=text, caption_entities=entities, **kwargs
+            )
+        sent = True
+    except:
+        pass
+    return sent
