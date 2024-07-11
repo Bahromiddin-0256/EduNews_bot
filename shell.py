f822dc2e0d7ea0ab51a250cff11bdea63e17bdbb
bahromiddin <b.ibragimov@tuit.uz>
2024-07-11 23:53:33 +0500

    chore: Refactor shell.py for IPython compatibility and database initialization

--- shell.py
+++ shell.py
@@ -5,21 +5,13 @@ from core.config import settings, TORTOISE_ORM
 async def init():
     await Tortoise.init(config=TORTOISE_ORM)
     await Tortoise.generate_schemas()
-    # Your other initialization code here
+    from db.models import Admin
 
-# Adjusting the event loop for environments like IPython
-try:
-    # Attempt to get the current running loop; if not found, create a new one.
-    loop = asyncio.get_running_loop()
-except RuntimeError:
-    # If no running loop is found, create a new event loop and set it as the current one.
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-# Run the init coroutine using the adjusted event loop
+# Run the init coroutine
+loop = asyncio.get_event_loop()
 loop.run_until_complete(init())
 
-# Continue with IPython or standard shell
+# If using IPython, you can start it here, or just use the standard shell
 try:
     from IPython import start_ipython
     start_ipython(argv=[])
